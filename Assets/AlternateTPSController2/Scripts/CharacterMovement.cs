f91f48ebd42b8726a8b3d66b688ba6f9bf53a129
DmitryKotusev <dssa-1966@yandex.ru>
2019-04-20 00:11:16 +0300

    Started new animation branch.

---
+++ Assets/AlternateTPSController2/Scripts/CharacterMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    Animator anim;
+    [SerializeField]
+    float vertical;
+    [SerializeField]
+    float horizontal;
+    public float moveAmount;
+
+    public void MoveUpdate()
+    {
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        moveAmount = Mathf.Clamp01(Mathf.Abs(vertical) + Mathf.Abs(horizontal));
+
+        anim.SetFloat("vertical", vertical, 0.15f, Time.deltaTime);
+        anim.SetFloat("horizontal", horizontal, 0.15f, Time.deltaTime);
+    }
+}
