bb767f99716c55f9cd601172702fc48aefbe30c7
DmitryKotusev <dssa-1966@yandex.ru>
2019-04-30 00:57:23 +0300

    Made new camera controller.

---
+++ Assets/AlternateTPSController2/Scripts/properties/CharacterStatus.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Character/Status")]
+public class CharacterStatus : ScriptableObject
+{
+    public bool isAiming;
+    public bool isSprinting;
+    public bool isGround;
+}
