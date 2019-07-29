40dc3b9b3baee69de2986fcef50a5074b64abc0d
DmitryKotusev <dssa-1966@yandex.ru>
2019-07-29 23:46:19 +0300

    Did some stuff.

--- Assets/AlternateTPSController2/Scripts/CharacterMovement.cs
+++ Assets/AlternateTPSController2/Scripts/CharacterMovement.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     [SerializeField]
+    Transform cameraTransform;
+    [SerializeField]
+    CharacterStatus characterStatus;
+    [SerializeField]
     Animator anim;
     [SerializeField]
     float vertical;
     [SerializeField]
     float horizontal;
+    [SerializeField]
+    float rotationSpeed = 100f;
     public float moveAmount;
+
+    Vector3 rotationDirection;
+    Vector3 moveDirection;
 
     public void MoveUpdate()
     {
@@ -18,7 +27,35 @@ public class CharacterMovement : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         moveAmount = Mathf.Clamp01(Mathf.Abs(vertical) + Mathf.Abs(horizontal));
 
-        anim.SetFloat("vertical", vertical, 0.15f, Time.deltaTime);
-        anim.SetFloat("horizontal", horizontal, 0.15f, Time.deltaTime);
+        anim.SetFloat("vertical", moveAmount, 0.15f, Time.deltaTime);
+        // anim.SetFloat("horizontal", horizontal, 0.15f, Time.deltaTime);
+
+        Vector3 moveDir = cameraTransform.forward * vertical;
+        moveDir += cameraTransform.right * horizontal;
+        moveDir.Normalize();
+        moveDirection = moveDir;
+        rotationDirection = cameraTransform.forward;
+
+        RotationNormal();
+    }
+
+    public void RotationNormal()
+    {
+        if (!characterStatus.isAiming)
+        {
+            rotationDirection = moveDirection;
+        }
+
+        Vector3 targetDirection = rotationDirection;
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+        {
+            targetDirection = transform.forward;
+        }
+
+        Quaternion lookDir = Quaternion.LookRotation(targetDirection);
+        Quaternion targetRot = Quaternion.Slerp(transform.rotation, lookDir, rotationSpeed * Time.deltaTime);
+        transform.rotation = targetRot;
     }
 }
